9b5486480e3265ad5696013987216447dbc7bcac
Alexander Tiedmann <s0556127@htw-berlin.de>
2018-05-31 15:29:56 +0200

    Platzhalter zur compilierung der Tests

--- src/main/scala/wordcount/Processing.scala
+++ src/main/scala/wordcount/Processing.scala
@@ -51,7 +51,7 @@ class Processing {
     l.foldLeft( //links falten
       Map.empty[String, Int]){ //leere Map erstellen
       (count, word) => count + // Map fuellen und die Count Zahl erhohen
-        (word -> (count.getOrElse(word, 0) + 1))} // get word und +1 wenn Wort nicht vorhanden ansonsten +1
+        (word -> (count.getOrElse(word, 0) + 1))} // get word +0 wenn Wort nicht vorhanden ansonsten +1 danach +1 da Wort ja einmal min. da
       .toList //map in liste [String, Int] umwandeln
   }
 
@@ -65,11 +65,11 @@ class Processing {
   def mapReduce[S,B,R](mapFun:(S=>B),
       redFun:(R,B)=>R,
       base:R,
-      l:List[S]):R =
+      l:List[S]):R = l.map(mapFun).foldLeft(base)(redFun)
 
-  l.map(mapFun).foldLeft(base)(redFun)
-
-  def countTheWordsMR(l:List[String]):List[(String,Int)]= ???
+  def countTheWordsMR(l:List[String]):List[(String,Int)]= {
+     Map[String, Int]().empty.toList
+  }
 
 
   /**********************************************************************************************
@@ -79,7 +79,9 @@ class Processing {
    *********************************************************************************************
   */
 
-    def getAllWordsWithIndex(l:List[(Int,String)]):List[(Int,String)]= ???
+    def getAllWordsWithIndex(l:List[(Int,String)]):List[(Int,String)]= {
+      Map.empty[Int, String].toList
+    }
 
     /*
      * Extracts all Words from a List containing tupels consisting
@@ -87,19 +89,16 @@ class Processing {
      */
 
 
-   def createInverseIndex(l:List[(Int,String)]):Map[String,List[Int]]={
-
-     ???
+   def createInverseIndex(l:List[(Int,String)]):Map[String,List[Int]]= {
+     Map[String, List[Int]]().empty
    }
 
    def andConjunction(words:List[String], invInd:Map[String,List[Int]]):List[Int]={
-
-     ???
+     Map.empty[String, Int].valuesIterator.toList
    }
 
    def orConjunction(words:List[String], invInd:Map[String, List[Int]]):List[Int]={
-
-     ???
+     Map.empty[String, Int].valuesIterator.toList
    }
 }
 
